d2953859a20ea7f941174f48fcb28d36cf6af912
Richard Barbour <richyb07@gmail.com>
2023-08-18 12:24:13 -0700

    Pass test 3.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,25 @@
 // Business Logic
 
-function stringToInt(userInput) {
+let verifiedInt;
 
-  let verifiedInt;
+function stringToInt(userInput) {
 
   if (isNaN(parseInt(userInput))) {
     //Will change this return later
     return "Please input a number using Arabic numerals 0-9."
-  } else {
-    return verifiedInt = parseInt(userInput);
+  } 
+  else {
+    return verifiedInt = parseInt(userInput)
   }
+
 }
+
+function intToArray() {
+  
+  let intArray = Array.from({length: verifiedInt + 1}, (value, index) => index);
+  return intArray;
+
+}
+
+
+
